bd1615e95cd1f6ff227ae6d079aecccb524d42b8
DHV <dhv@gmail.com>
2020-10-12 09:44:30 +0700

    Using AnimationContainer lv1

--- learn_flutter/lib/main.dart
+++ learn_flutter/lib/main.dart
@@ -20,8 +20,23 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({Key key}) : super(key: key);
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  double _width = 200;
+  double _height = 200;
+
+  void _updateState() {
+    setState(() {
+      _width = 400;
+      _height = 400;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -29,7 +44,32 @@ class HomePage extends StatelessWidget {
       appBar: AppBar(
         title: Text('Flutter'),
       ),
-      body: Container(),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            RaisedButton(
+              onPressed: () {
+                _updateState();
+              },
+              child: Text("Animate"),
+            ),
+            AnimatedContainer(
+              duration: Duration(milliseconds: 400),
+              curve: Curves.easeInOut,
+              height: _width,
+              width: _height,
+              color: Colors.lightBlue[200],
+              child: Center(
+                child: Text(
+                  "Animation",
+                  style: Theme.of(context).textTheme.headline5,
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
